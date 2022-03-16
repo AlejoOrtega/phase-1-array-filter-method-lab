3c5970b398a0182b776e09ded9a2f7c402d2ede8
Alejandro <ajrevollo@uninorte.edu.co>
2022-03-16 17:25:49 -0400

    Lab Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+let findMatching = (array, name) => {
+    let response = array.filter(driver => driver.toLowerCase() === name.toLowerCase())
+    return response !== undefined ? response : undefined;
+}
+
+const fuzzyMatch = (array, letters) => {
+    
+    let response = array.filter(driver => driver.startsWith(letters))
+    return response !== undefined ? response : undefined;
+}
+
+const matchName = (array, name) => {
+    let response = array.filter(driver => driver.name === name);
+    return response !== undefined ? response : undefined;
+}
